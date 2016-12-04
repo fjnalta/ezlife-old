17b0f5239896eed89a83c113ee8b87491fff9bd6
fjnalta <able@ezlife.eu>
2016-12-04 22:37:01 +0100

    implement show only last 10 commits fixed errors

--- js/gitlab-rss.js
+++ js/gitlab-rss.js
@@ -1,6 +1,8 @@
 var feed = "https://ezlife.eu/apps/gitlab/philippm/ezlife.eu/commits/master.atom";
+var i = 0;
 
 function createEntry(link, title, date) {
+    "use strict";
     var myFeed = document.createElement("P");
     var mylink = document.createElement("A");
     mylink.setAttribute('href', link);
@@ -12,13 +14,14 @@ function createEntry(link, title, date) {
 }
 
 function loadezlifeRSS() {
+    "use strict";
     $.ajax(feed, {
         accepts: {
             xml: "application/xml"
         },
         dataType: "xml",
         success: function (data) {
-            $(data).find("entry").each(function () {
+            $(data).find("entry").slice(0, 10).each(function () {
                 var el = $(this);
                 createEntry(el.find("id").text(), el.find("title").text(), el.find("updated").text());
             });
