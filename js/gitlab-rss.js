e6c6107a33e489d2812b6d15007ad546a3671922
Philipp Minges <able@ezlife.eu>
2016-12-04 20:11:17 +0100

    implement RSS Feeds update Development Feed

---
+++ js/gitlab-rss.js
@@ -0,0 +1,30 @@
+var feed = "https://ezlife.eu/apps/gitlab/philippm/ezlife.eu/commits/master.atom";
+
+function createEntry(link, title, date) {
+    var myFeed = document.createElement("P");
+    var mylink = document.createElement("A");
+    mylink.setAttribute('href', link);
+    mylink.appendChild(document.createTextNode(title));
+    myFeed.appendChild(document.createTextNode(date));
+    myFeed.appendChild(document.createTextNode(" - "))
+    myFeed.appendChild(mylink);
+    document.getElementById("ezlifeRSS").appendChild(myFeed);
+}
+
+function loadezlifeRSS() {
+    $.ajax(feed, {
+        accepts: {
+            xml: "application/rss+xml"
+        },
+        dataType: "xml",
+        success: function (data) {
+            $(data).find("entry").each(function () {
+                var el = $(this);
+                createEntry(el.find("link").text(), el.find("title").text(), el.find("updated").text());
+            });
+        }
+    });
+}
+
+
+
