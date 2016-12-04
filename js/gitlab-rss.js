2cdadf0ba14139c7bf3139d428aa6d3d46ca2473
Philipp Minges <able@ezlife.eu>
2016-12-04 18:28:28 +0100

    bugfixes

---
+++ js/gitlab-rss.js
@@ -0,0 +1,10 @@
+$.get("https://ezlife.eu/apps/gitlab/philippm/ezlife.eu/commits/master.atom", function (data) {
+    $(data).find("entry").each(function () { // or "item" or whatever suits your feed
+        var el = $(this);
+
+        console.log("------------------------");
+        console.log("title      : " + el.find("title").text());
+        console.log("author     : " + el.find("author").text());
+        console.log("description: " + el.find("description").text());
+    });
+});
