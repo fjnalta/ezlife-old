b3e195d8d294a0e91cc092c474799ae809a1d35f
Philipp Minges <able@ezlife.eu>
2017-01-17 16:47:38 +0100

    implement captcha logic

--- js/chatRegistration.js
+++ js/chatRegistration.js
@@ -1,3 +1,18 @@
+// always check for Password Match
+$(document).ready(function () {
+    "use strict";
+    $("#password, #password2").keyup(checkPasswordMatch);
+});
+
+// Asynchronous Google-Captcha loading - body
+var loadCaptcha = function () {
+    "use strict";
+	grecaptcha.render('captcha', {
+        'sitekey' : '6LerExIUAAAAAFxTCbvdsNpOUrsXfxg7yqMniZQT'
+    });
+};
+loadCaptcha();
+
 function setRegistrationState(state) {
     "use strict";
     switch (state) {
@@ -7,11 +22,15 @@ function setRegistrationState(state) {
     case "fail":
         $("#divCheckRegistration").html("Failed! User not created");
         break;
+    case "failcaptcha":
+        $("#divCheckRegistration").html("Failed! User not created - Captcha wrong");
+        break;
     default:
         break;
     }
 }
 
+// User Registration
 function registerUser() {
     "use strict";
     // REST-API URL
@@ -19,33 +38,40 @@ function registerUser() {
     
     // Get Variables from HTML-Page
     var user = document.getElementById('username').value;
+    var nick = document.getElementById('nickname').value;
     var password = document.getElementById('password').value;
     var password2 = document.getElementById('password2').value;
     var email = document.getElementById('email').value;
     
-    // create JSON from data
-    if (password == password2) {
-        var data = JSON.stringify({
-            "username": user,
-            "password": password,
-            "email": email
-        });
-    
-        // Start REST-POST
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/json");
-        xhr.setRequestHeader("Authorization", "kAmkjv3879kK0v82");
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 201) {
-                setRegistrationState("success");
-            } else {
-                setRegistrationState("fail");
-            }
-        };
-        xhr.send(data);
+    // check for captcha first
+    if (grecaptcha.getResponse() == "") {
+        setRegistrationState("failcaptcha");
     } else {
-        setRegistrationState("fail");
+        // create JSON from data if values match
+        if (password === password2 && password.length > 7 && user.length > 2 && user !== "" && nick !== "") {
+            var data = JSON.stringify({
+                "username": user,
+                "password": password,
+                "name": nick,
+                "email": email
+            });
+    
+            // Start REST-POST
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.setRequestHeader("Authorization", "kAmkjv3879kK0v82");
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 201) {
+                    setRegistrationState("success");
+                } else {
+                    setRegistrationState("fail");
+                }
+            };
+            xhr.send(data);
+        } else {
+            setRegistrationState("fail");
+        }
     }
 }
 
@@ -54,7 +80,7 @@ function checkPasswordMatch() {
     var password = $("#password").val();
     var confirmPassword = $("#password2").val();
     
-    if (password != confirmPassword) {
+    if (password !== confirmPassword) {
         $("#password2").css("background-color", "#ff6666");
         $("#divCheckPasswordMatch").html("Passwords do not match!");
     } else {
@@ -62,8 +88,3 @@ function checkPasswordMatch() {
         $("#divCheckPasswordMatch").html("Passwords match.");
     }
 }
-
-$(document).ready(function () {
-    "use strict";
-    $("#password, #password2").keyup(checkPasswordMatch);
-});
